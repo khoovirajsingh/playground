db521aa94d9b993894c0d708be43077dbd2c6e18
khoovis <khoovirajsingh.seewoonauth@mkodo.com>
2020-02-06 21:02:06 +0000

    learn fundamentals

--- src/playground/core.clj
+++ src/playground/core.clj
@@ -5,3 +5,46 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(str "REPL" "is" "cool")
+
+(str "Tim \"tpope\" Pope")
+
+(println "Tim \"tpope\" Pope")
+
+(prn "Tim \"tpope\" Pope")
+
+(+ 10 10 5)
+
+(+ 3)
+
+(+ 10 5.5)
+
+(- 10 3 2)
+
+(- 3 7)
+
+(* 10 5)
+
+(/ 10 5)
+
+(/ 1 3)
+
+(+ 1 (/ 1 3))
+
+(type 3)
+
+(nil? nil)
+
+(true? (> 1 0))
+
+'(1 2 3 "hello")
+
+[1 2 3 "bye"]
+
+{:name "Venkat" :age 51 :job "Developer"}
+
+{3 "is a number" "person" "khoovi"}
+
+(coll? #{"apple" "orange"})
+
